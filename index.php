9b24f1eaa73c85e0e56cf7a92c4fcc0207978b1c
Thomas VB <van.bouwel.thomas@gmail.com>
2016-02-15 19:26:30 +0100

    landing page css & bootstrap clusterfuck

---
+++ index.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+            body {
+                padding-bottom: 20px;
+            }
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/style.css">
+		
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <!-- Nav -->
+	    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	      <div class="container">
+	      	
+	        <div class="navbar-header">
+        		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>                        
+		      	</button>
+	          	<img src="img/LogoSmall.png" class="logo"/>
+	        </div>
+	        
+	        <div class="collapse navbar-collapse" id="myNavbar">
+			      <ul class="nav navbar-nav navbar-right">
+			        <li class="menuoption"><a href="#" class="white"><h4>ABOUT</h4></a></li>
+			        <li class="menuoption"><a href="#" class="white"><h4>EVENTS</h4></a></li>
+			        <li class="menuoption"><a href="#" class="white"><h4>NEWS</h4></a></li>
+			      </ul>
+			</div>
+	        
+	      </div>
+	    </nav>
+
+		<!-- HomePage CoverImage-->
+	    <div class="coverimage">
+	    
+		    <nav class="navbar navbar-inverse" role="navigation">
+		      	
+			        <div class="navbar-header">
+		        		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar2">
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>                        
+				      	</button>
+			        </div>
+			        
+			        <div class="collapse navbar-collapse" id="myNavbar2">
+					      <ul class="nav navbar-nav navbar-right">
+					        <li class="menuoption"><a href="#" class="white"><h4>ABOUT</h4></a></li>
+					        <li class="menuoption"><a href="#" class="white"><h4>EVENTS</h4></a></li>
+					        <li class="menuoption"><a href="#" class="white"><h4>NEWS</h4></a></li>
+					      </ul>
+					</div>
+		        
+		    </nav>
+	    	
+		  <div class="inner">
+			<div class="content">
+				<img src="./img/Logo.png" class="img-responsive" />
+				<h1 class="white">Website Under Construction</h1>
+			</div>
+		  </div>
+		  
+	    </div>
+	    
+	    <!-- About Short -->
+
+		
+	    <div class="container">
+	
+	      <div class="row">
+	        <div class="col-md-4">
+	          <h2>Heading</h2>
+	          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+	          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+	        </div>
+	        <div class="col-md-4">
+	          <h2>Heading</h2>
+	          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+	          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+	       </div>
+	        <div class="col-md-4">
+	          <h2>Heading</h2>
+	          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+	        </div>
+	      </div>
+	      
+	      <div>
+	      	<p>
+	      		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at elit quis nisi lacinia imperdiet sit amet at enim. Integer auctor quis nulla et lacinia. Proin et sem luctus, consequat diam at, sollicitudin magna. Aliquam aliquam tincidunt mauris. Praesent tincidunt vel dui at consequat. Integer pharetra purus arcu, sit amet mollis leo dictum et. Proin viverra tellus urna, nec porta massa maximus vitae. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Fusce quis turpis vel velit dignissim luctus. Phasellus lobortis ipsum vitae dolor gravida, faucibus egestas sem mollis. Quisque in erat eget metus malesuada condimentum. Sed eu tortor sed justo aliquam efficitur nec vitae ex. Sed et orci nibh. Proin vestibulum libero ac dui tempor, eget lobortis ligula tempus. Donec ut elit et urna pellentesque consequat vel eget felis. Morbi efficitur euismod lorem, eu mollis tellus scelerisque blandit. Nulla faucibus risus a ultricies dictum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce lobortis, est at dapibus gravida, urna metus malesuada neque, ut ultrices ex mauris non mi. Ut imperdiet a urna vitae ornare.
+Cras ornare lorem a leo pretium finibus.
+			</p>
+			<p>
+	      		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at elit quis nisi lacinia imperdiet sit amet at enim. Integer auctor quis nulla et lacinia. Proin et sem luctus, consequat diam at, sollicitudin magna. Aliquam aliquam tincidunt mauris. Praesent tincidunt vel dui at consequat. Integer pharetra purus arcu, sit amet mollis leo dictum et. Proin viverra tellus urna, nec porta massa maximus vitae. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Fusce quis turpis vel velit dignissim luctus. Phasellus lobortis ipsum vitae dolor gravida, faucibus egestas sem mollis. Quisque in erat eget metus malesuada condimentum. Sed eu tortor sed justo aliquam efficitur nec vitae ex. Sed et orci nibh. Proin vestibulum libero ac dui tempor, eget lobortis ligula tempus. Donec ut elit et urna pellentesque consequat vel eget felis. Morbi efficitur euismod lorem, eu mollis tellus scelerisque blandit. Nulla faucibus risus a ultricies dictum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce lobortis, est at dapibus gravida, urna metus malesuada neque, ut ultrices ex mauris non mi. Ut imperdiet a urna vitae ornare.
+Cras ornare lorem a leo pretium finibus.
+			</p>
+			<p>
+	      		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at elit quis nisi lacinia imperdiet sit amet at enim. Integer auctor quis nulla et lacinia. Proin et sem luctus, consequat diam at, sollicitudin magna. Aliquam aliquam tincidunt mauris. Praesent tincidunt vel dui at consequat. Integer pharetra purus arcu, sit amet mollis leo dictum et. Proin viverra tellus urna, nec porta massa maximus vitae. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Fusce quis turpis vel velit dignissim luctus. Phasellus lobortis ipsum vitae dolor gravida, faucibus egestas sem mollis. Quisque in erat eget metus malesuada condimentum. Sed eu tortor sed justo aliquam efficitur nec vitae ex. Sed et orci nibh. Proin vestibulum libero ac dui tempor, eget lobortis ligula tempus. Donec ut elit et urna pellentesque consequat vel eget felis. Morbi efficitur euismod lorem, eu mollis tellus scelerisque blandit. Nulla faucibus risus a ultricies dictum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce lobortis, est at dapibus gravida, urna metus malesuada neque, ut ultrices ex mauris non mi. Ut imperdiet a urna vitae ornare.
+Cras ornare lorem a leo pretium finibus.
+			</p>
+			<p>
+	      		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at elit quis nisi lacinia imperdiet sit amet at enim. Integer auctor quis nulla et lacinia. Proin et sem luctus, consequat diam at, sollicitudin magna. Aliquam aliquam tincidunt mauris. Praesent tincidunt vel dui at consequat. Integer pharetra purus arcu, sit amet mollis leo dictum et. Proin viverra tellus urna, nec porta massa maximus vitae. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Fusce quis turpis vel velit dignissim luctus. Phasellus lobortis ipsum vitae dolor gravida, faucibus egestas sem mollis. Quisque in erat eget metus malesuada condimentum. Sed eu tortor sed justo aliquam efficitur nec vitae ex. Sed et orci nibh. Proin vestibulum libero ac dui tempor, eget lobortis ligula tempus. Donec ut elit et urna pellentesque consequat vel eget felis. Morbi efficitur euismod lorem, eu mollis tellus scelerisque blandit. Nulla faucibus risus a ultricies dictum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce lobortis, est at dapibus gravida, urna metus malesuada neque, ut ultrices ex mauris non mi. Ut imperdiet a urna vitae ornare.
+Cras ornare lorem a leo pretium finibus.
+			</p>
+			<p>
+	      		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at elit quis nisi lacinia imperdiet sit amet at enim. Integer auctor quis nulla et lacinia. Proin et sem luctus, consequat diam at, sollicitudin magna. Aliquam aliquam tincidunt mauris. Praesent tincidunt vel dui at consequat. Integer pharetra purus arcu, sit amet mollis leo dictum et. Proin viverra tellus urna, nec porta massa maximus vitae. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Fusce quis turpis vel velit dignissim luctus. Phasellus lobortis ipsum vitae dolor gravida, faucibus egestas sem mollis. Quisque in erat eget metus malesuada condimentum. Sed eu tortor sed justo aliquam efficitur nec vitae ex. Sed et orci nibh. Proin vestibulum libero ac dui tempor, eget lobortis ligula tempus. Donec ut elit et urna pellentesque consequat vel eget felis. Morbi efficitur euismod lorem, eu mollis tellus scelerisque blandit. Nulla faucibus risus a ultricies dictum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce lobortis, est at dapibus gravida, urna metus malesuada neque, ut ultrices ex mauris non mi. Ut imperdiet a urna vitae ornare.
+Cras ornare lorem a leo pretium finibus.
+			</p>
+			<p>
+	      		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at elit quis nisi lacinia imperdiet sit amet at enim. Integer auctor quis nulla et lacinia. Proin et sem luctus, consequat diam at, sollicitudin magna. Aliquam aliquam tincidunt mauris. Praesent tincidunt vel dui at consequat. Integer pharetra purus arcu, sit amet mollis leo dictum et. Proin viverra tellus urna, nec porta massa maximus vitae. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Fusce quis turpis vel velit dignissim luctus. Phasellus lobortis ipsum vitae dolor gravida, faucibus egestas sem mollis. Quisque in erat eget metus malesuada condimentum. Sed eu tortor sed justo aliquam efficitur nec vitae ex. Sed et orci nibh. Proin vestibulum libero ac dui tempor, eget lobortis ligula tempus. Donec ut elit et urna pellentesque consequat vel eget felis. Morbi efficitur euismod lorem, eu mollis tellus scelerisque blandit. Nulla faucibus risus a ultricies dictum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce lobortis, est at dapibus gravida, urna metus malesuada neque, ut ultrices ex mauris non mi. Ut imperdiet a urna vitae ornare.
+Cras ornare lorem a leo pretium finibus.
+			</p>
+	      </div>
+	
+	      <hr>
+	
+	      <footer>
+	        <p>&copy; Binair01 2015</p>
+	      </footer>
+	    </div> 
+	    
+    	<!-- /container -->        
+    	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
